e379560d8d935ef0ac3b80c6ac4560daa3cfb4e8
Dommert <DommertEnterprises@Gmail.com>
2013-10-24 02:49:17 -0700

    update to index
    :)

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 echo 'Hello World <BR>';
 echo 'Welcome to GitHub <BR>';
-echo 'Dommert is a horrible teach, just saying...';
+// echo 'Dommert is a horrible teach, just saying...';
+echo 'Dommert is a great teacher. I just learned how to use Git - Kam'
 
 ?>
